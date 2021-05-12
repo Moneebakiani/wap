462087f798135dd6bd36a698fae017bedc30d531
MoneebaKiani <moneebakiani94@gmail.com>
2021-05-11 21:57:00 -0500

    lab15

---
+++ lab15/guestListAjax/target/guestlistAjax-1.0-SNAPSHOT/resources/js/guests.js
@@ -0,0 +1,30 @@
+$(function() {
+    $("#send").click(updateGuests);
+});
+
+function updateGuests() {
+
+    var first = $("#first").val();
+    var last = $("#last").val();
+
+    $.ajax("guest.ajax", {
+		"type": "post",
+		"data": {
+        	"first": first,
+                "last": last
+		}
+    }).done(displayGuests);
+}
+
+function displayGuests(data) {
+
+    var guestList = "You need to modify this method to display the updated guest list.  Remember to build the entire list before adding it to the DOM.";
+console.log(data);
+    $("#guestList").text("");
+
+    data.forEach(function(e,i){
+        $("#guestList").append($("<ul>").text((i+1)+") "+e.first+" "+e.last))
+    });
+   // $("#guestList").html(guestList);
+    
+}
